9374add6d6ecbfa4e9e060e6786f3bf4f45c599f
SanketMN <skt_nit@yahoo.com>
2024-10-24 12:49:35 +0530

    Add files via upload

---
+++ Descriptive_Stats.R
@@ -0,0 +1,54 @@
+
+View(df)
+
+
+####Top 10 rows and last 10 rows
+
+head(df)
+head(df,1)
+tail(df)
+tail(df,25)
+
+###########Summary of the data#########
+
+summary(df)
+
+df$Ozone
+Ozone
+Wind
+summary(df$Ozone)
+attach(df)
+Month
+Ozone
+
+###########DATA VISUALIZATION IN R##########
+#Scatter plot
+
+plot(x = Ozone, y = Temp)
+
+plot(Ozone,Wind, xlab = 'Ozone Levels',ylab = 'Wind Values',
+     main = 'Scatter Plot Between Ozone and Wind',
+     col = 'blue',pch = 19)
+
+#Plot entire df
+plot(df)
+
+#Horizontal bar plot
+
+barplot(Month)
+unique(Month)
+fre <- table(Month)
+fre
+barplot(fre,col = 'darkred')
+table(Month)
+
+#Histogram
+hist(Ozone)
+hist(Temp)                
+hist(Wind)
+
+##Single box plot and stats
+boxplot(Ozone, col = 'blue')
+# Multiple box plots
+boxplot(df)
+
